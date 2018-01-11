d530eb3238a11a9e0ea2c3c5f2c55ed8205649c8
Matthew Appleby <mappleby1@me.com>
2018-01-11 15:52:56 -0500

    created merge function

--- mergesort.js
+++ mergesort.js
@@ -1,9 +1,29 @@
 'use strict'
 function split(wholeArray) {
-
-  /* your code here to define the firstHalf and secondHalf arrays */
   var splitPos = Math.floor(wholeArray.length / 2)
-  var firstHalf = wholeArray.slice(0 , splitPos - 1);
+  var firstHalf = wholeArray.slice(0 , splitPos);
   var secondHalf = wholeArray.slice(splitPos);
   return [firstHalf, secondHalf];
 }
+
+function merge(arr1, arr2){
+  var sortedArr = [];
+  var arr1Copy = arr1.slice();
+  var arr2Copy = arr2.slice();
+
+  while (arr1Copy.length && arr2Copy.length){
+    if (!arr1Copy.length){
+      sortedArr = sortedArr.concat(arr2Copy);
+      arr2Copy = [];
+    } else if (!arr2Copy.length){
+      sortedArr = sortedArr.concat(arr1Copy);
+      arr1Copy = [];
+    } else if (arr1Copy[0] < arr2Copy[0]){
+      sortedArr.push(arr1Copy.shift());
+    } else {
+      sortedArr.push(arr2Copy.shift());
+    }
+  }
+  return sortedArr;
+}
+
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,5 +1,26 @@
 describe('Split Array function', function() {
-  it('is able to split an array into two halves', function() {
-    // your code here
+  it('is able to split an even array into two halves', function() {
+  expect(split([1,2,3,4])).toEqual([[1,2],[3,4]]);
   });
+
+  it('is able to split an odd array into two halves', function() {
+    expect(split([1,2,3,4,5])).toEqual([[1,2],[3,4,5]]);
+    });
 });
+
+/*
+describe('Bubble Sort Count', function(){
+  beforeEach(function(){
+    spyOn(window, 'swap').and.callThrough();
+  });
+  it('Fully sorted no swap', function(){
+    bubbleSort([1,2,3]);
+    expect(window.swap.calls.count()).toEqual(0);
+  });
+  it('Fully sorted no swap', function(){
+    bubbleSort([3,2,1]);
+    expect(window.swap.calls.count()).toEqual(3);
+  });
+
+
+});*/

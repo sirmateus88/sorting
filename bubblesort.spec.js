b9628ecf69dedf43fde410f93533d28e2e253e35
Matthew Appleby <mappleby1@me.com>
2018-01-11 11:42:16 -0500

    added 5 tests

---
+++ bubblesort.spec.js
@@ -0,0 +1,23 @@
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('handles a 1 item array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('handles an already sorted array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('handles a randomly sorted array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('handles a worst case array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+
+});

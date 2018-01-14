3c1b7ea3b793915089366136cc55a277a0db04cc
Matthew Appleby <mappleby1@me.com>
2018-01-14 12:57:55 -0500

    solved merge sort

--- mergesort.js
+++ mergesort.js
@@ -1,7 +1,8 @@
 'use strict'
+
 function split(wholeArray) {
   var splitPos = Math.floor(wholeArray.length / 2)
-  var firstHalf = wholeArray.slice(0 , splitPos);
+  var firstHalf = wholeArray.slice(0, splitPos);
   var secondHalf = wholeArray.slice(splitPos);
   return [firstHalf, secondHalf];
 }
@@ -10,14 +11,11 @@ function merge(arr1, arr2){
   var sortedArr = [];
   var arr1Copy = arr1.slice();
   var arr2Copy = arr2.slice();
-
-  while (arr1Copy.length && arr2Copy.length){
+  while (arr1Copy.length || arr2Copy.length){
     if (!arr1Copy.length){
-      sortedArr = sortedArr.concat(arr2Copy);
-      arr2Copy = [];
+      return sortedArr.concat(arr2Copy);
     } else if (!arr2Copy.length){
-      sortedArr = sortedArr.concat(arr1Copy);
-      arr1Copy = [];
+      return sortedArr.concat(arr1Copy);
     } else if (arr1Copy[0] < arr2Copy[0]){
       sortedArr.push(arr1Copy.shift());
     } else {
@@ -27,3 +25,39 @@ function merge(arr1, arr2){
   return sortedArr;
 }
 
+function mergeSort(arr){
+  //call split until you get an array of single arrays
+  //call merge on two of these arrays at a time until all merged together
+  var holdArr = [];
+  var singleArrs = split(arr);
+  while (!singleArrCheck(singleArrs)){
+    for (let i = 0; i < singleArrs.length; i++){
+      if(singleArrs[i].length > 1){
+        holdArr = split(singleArrs[i]);
+        singleArrs.splice(i, 1, holdArr[0], holdArr[1]);
+        i++;
+      }
+    }
+  }
+
+  var arrNext;
+  while (singleArrs.length > 1){
+    for (let k = 0; k < singleArrs.length; k++){
+      if(singleArrs[k + 1]){
+        arrNext = singleArrs.splice(k + 1, 1);
+        singleArrs[k] = merge(singleArrs[k], arrNext[0]);
+      }
+    }
+  }
+
+  return singleArrs[0];
+}
+
+//helper function
+function singleArrCheck(arr){
+  var allSingle = true;
+  arr.forEach(function(elem){
+    if (elem.length > 1) allSingle = false;
+  });
+  return allSingle;
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -8,19 +8,22 @@ describe('Split Array function', function() {
     });
 });
 
-/*
-describe('Bubble Sort Count', function(){
-  beforeEach(function(){
-    spyOn(window, 'swap').and.callThrough();
-  });
-  it('Fully sorted no swap', function(){
-    bubbleSort([1,2,3]);
-    expect(window.swap.calls.count()).toEqual(0);
-  });
-  it('Fully sorted no swap', function(){
-    bubbleSort([3,2,1]);
-    expect(window.swap.calls.count()).toEqual(3);
+describe('Merge Array function', function() {
+  it('is able to merge two sorted arrays into a larger sorted array', function() {
+  expect(merge([1,2],[3,4])).toEqual([1,2,3,4]);
   });
 
+  it('is able to merge two sorted arrays with intersperced values and different lengths', function() {
+    expect(merge([1,3,5,7,9],[2,4,6,8,10,12,14,16,18])).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18]);
+    });
+});
 
-});*/
+describe('Merge Sort function', function() {
+  it('is able to sort a reverse ordered even number array', function() {
+  expect(mergeSort([4,3,2,1])).toEqual([1,2,3,4]);
+  });
+
+  it('is able to sort a random ordered odd number length array', function() {
+    expect(mergeSort([1,100,3,5,7,9,2,4,6,8,10,12,14,16,18])).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 100]);
+    });
+});
